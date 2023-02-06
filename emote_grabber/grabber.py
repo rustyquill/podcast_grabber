00f94724d20fadffaf922c8ca91cee1e432a4784
askMartyn <martyn@automationlogic.com>
2023-02-06 22:37:05 +0000

    trying to get chromium working

--- emote_grabber/grabber.py
+++ emote_grabber/grabber.py
@@ -1,14 +1,18 @@
 import subprocess
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 
 class emote_grabber(object):
     def __init__(self, channel_name=None):
        self.options = Options()
-       #self.options.add_argument("--headless")
-       self.driver = webdriver.Firefox(options=self.options)
+       self.display = Display(visible=0, size=(1000, 1200))
+       self.options.add_argument("--headless")
+       # self.driver = webdriver.Firefox(options=self.options)
+       self.driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
        self.channel_name = channel_name
        self.channel_url = None
        self.emotes      = None 
