d961c04d22d7c83655d749a27c285f8da96964a5
Martyn Pratt <martyni@users.noreply.github.com>
2023-02-06 22:48:01 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 with open('requirements.txt') as requirements:
    setup(
       name		= 'emote_grabber',
-      version		= '2.6',
+      version		= '2.7',
       packages 		= ['emote_grabber'],
       package_dir	= {'': '.'},
       author		= 'askmartyn',
