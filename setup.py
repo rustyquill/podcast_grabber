6baecfe40fc8406cec3ac352f7d83844df9f8db4
askMartyn <martyn@automationlogic.com>
2023-02-20 18:35:03 +0000

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 with open('requirements.txt') as requirements:
    setup(
       name		= 'emote_grabber',
-      version		= '3.2',
+      version		= '3.3',
       packages 		= ['emote_grabber'],
       package_dir	= {'': '.'},
       author		= 'askmartyn',
